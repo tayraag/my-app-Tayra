c6ca43d9c998a116e7668f6cff33d96be8129c80
Tayra <tayraag07@gmail.com>
2026-05-11 22:45:50 -0300

    avances en ficha

--- app/fichas/[id].tsx
+++ app/fichas/[id].tsx
@@ -1,17 +1,40 @@
+import { productos, Producto } from "@/data/productos";
 import { Stack, useLocalSearchParams } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
-import "react-native-reanimated";
 
 type FichaParams = {
   id: string;
 };
 export default function FichaScreen() {
   const { id } = useLocalSearchParams<FichaParams>();
+  const prod = productos.find((p) => p.id === id);
+
+  if (!prod) {
+    return <Text>Producto no encontrado</Text>;
+  }
 
   return (
     <View style={styles.container}>
-      <Stack.Screen options={{title: "Producto" }}/>
-      <Text> Ficha del producto {id}</Text>
+      <Stack.Screen options={{ title: prod.nombre }} />
+      <DataPrincipal producto={prod}/>
+    </View>
+  );
+}
+
+function DataPrincipal ({producto} : {producto: Producto}){
+  return (
+    <View>
+      <Text>{producto.marca}</Text>
+      <Text>{producto.nombre}</Text>
+      <View style = {flexDirection: "row"}>
+        <Text style = {styles.scores}>{producto.nutriScore}</Text>
+        <Text style = {styles.scores}>{producto.novaGroup}</Text>
+        <Text style = {styles.scores}>{producto.ecoScore}</Text>
+      </View>
+      <View>
+        {// nutritional values
+        }
+      </View>
     </View>
   );
 }
@@ -23,6 +46,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     gap: 10,
     flex: 1,
+  },
+  scores:{
+
   },
 });
 
